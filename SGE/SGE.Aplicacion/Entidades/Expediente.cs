7ea2f46df15e43ca167380ba72375e280857c0dc
juanchisin <71896282+juanchii-coder@users.noreply.github.com>
2024-05-01 17:41:12 -0300

    xd

---
+++ SGE/SGE.Aplicacion/Entidades/Expediente.cs
@@ -0,0 +1,11 @@
+﻿namespace SGE.Aplicacion;
+
+public class Expediente
+{
+  int _Id;
+  String _Caratula;
+  DateTime FechaCreacion;
+  DateTime UltimaModificacion;
+  int _IdUsuario;//ultimo usuario que modifico el expediente
+  EstadoExpediente estado;
+}
--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e07250e565f25cf8bbb461802204a7b343970d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2f1d83a30424efab15705b3b99ec90bbbdb174a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
