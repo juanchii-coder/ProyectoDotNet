470a56a96eb7490d422fc223ff6a620aa9a0a797
juanchisin <71896282+juanchii-coder@users.noreply.github.com>
2024-05-03 17:46:53 -0300

    si

--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75e673535d6d04c90d668315e17add0bd8ce850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e433d51f9d0b209941153adf929a1d5dbac40aa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
