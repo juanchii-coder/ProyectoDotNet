18797e48f9e804b0f4d8f663b5c22078a5de0bb7
aaronPrieto26 <165234444+aaronPrieto26@users.noreply.github.com>
2024-05-12 16:37:52 -0300

    Update ServicioActualizacionEstado.cs

--- SGE/SGE.Aplicacion/Servicios/ServicioActualizacionEstado.cs
+++ SGE/SGE.Aplicacion/Servicios/ServicioActualizacionEstado.cs
@@ -2,5 +2,13 @@
 
 public class ServicioActualizacionEstado(ITramiteRepositorio repoTramite,IExpedienteRepositorio repoExpediente,EspecificacionCambioEstado especificacionCambioEstado)
 {
+   public void ActualizarEstado(int idExpediente)
+        {
+            var ultimoTramite = repoTramite.ConsultaPorId(idExpediente);
+            var nuevaEtiqueta = especificacionCambioEstado.ObtenerNuevaEtiqueta(ultimoTramite.ExpedienteId);
 
+            var expediente = repoExpediente.ConsultaPorId(idExpediente);
+            expediente.Estado = especificacionCambioEstado.ObtenerNuevoEstado(nuevaEtiqueta);
+            repoExpediente.Modificacion(expediente);
+        }
 }
