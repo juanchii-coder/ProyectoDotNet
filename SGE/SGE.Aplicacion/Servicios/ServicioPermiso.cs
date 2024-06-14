d4bbb3a25409e55ac16c116dc6a30a65afc1a8a5
Heraclio <ezequiel.heraclio.ortiz@gmail.com>
2024-06-14 15:35:01 -0300

    Añadiendo cambios a Eze-rama

--- SGE/SGE.Aplicacion/Servicios/ServicioPermiso.cs
+++ SGE/SGE.Aplicacion/Servicios/ServicioPermiso.cs
@@ -1,8 +1,56 @@
-﻿namespace SGE.Aplicacion;
+﻿using System.Collections.Generic;
+using SGE.Entidades;
+using SGE.Repositorios;
 
-public class ServicioAutorizacionProvisorio : IServicioAutorizacion
+namespace SGE.Servicios
 {
-    public bool PoseeElPermiso(int idUsuario, Permiso permiso) {
-        return idUsuario == 1;
+    public class ServicioPermiso : IServicioPermiso
+    {
+        private readonly IPermisosRepositorio permisoRepo;
+
+        public ServicioPermiso(IPermisosRepositorio permisosRepositorio)
+        {
+            permisoRepo = permisosRepositorio;
+        }
+
+        public void AsignarPermisoUsuario(int usuarioId, string permisoNombre)
+        {
+            permisoRepo.AsignarPermisoUsuario(usuarioId, permisoNombre);
+        }
+
+        public void RemoverPermisoUsuario(int usuarioId, string permisoNombre)
+        {
+            permisoRepo.RemoverPermisoUsuario(usuarioId, permisoNombre);
+        }
+
+        public bool UsuarioTienePermiso(int usuarioId, string permisoNombre)
+        {
+            return permisoRepo.UsuarioTienePermiso(usuarioId, permisoNombre);
+        }
+
+        public List<Permiso> PermisosDeUsuario(int usuarioId)
+        {
+            return permisoRepo.PermisosDeUsuario(usuarioId);
+        }
+
+        public void AgregarPermiso(Permiso permiso)
+        {
+            permisoRepo.AgregarPermiso(permiso);
+        }
+
+        public void ModificarPermiso(Permiso permiso)
+        {
+            permisoRepo.ModificarPermiso(permiso);
+        }
+
+        public void BorrarPermiso(string permisoNombre)
+        {
+            permisoRepo.BorrarPermiso(permisoNombre);
+        }
+
+        public List<int> UsuariosConPermiso(string permisoNombre)
+        {
+            return permisoRepo.UsuariosConPermiso(permisoNombre);
+        }
     }
 }
