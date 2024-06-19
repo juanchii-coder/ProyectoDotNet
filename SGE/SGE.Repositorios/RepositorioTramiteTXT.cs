278530e4bbc4c4cd6e15d9bbeb31212ffc923b1a
aaronPrieto26 <165234444+aaronPrieto26@users.noreply.github.com>
2024-06-18 21:00:49 -0300

    Update RepositorioTramiteTXT.cs

--- SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
@@ -1,130 +1,67 @@
-﻿using SGE.Aplicacion;
+using SGE.Aplicacion;
 using SGE.Aplicacion.Interfaces;
 using SGE.Aplicacion.Entidades;
 using SGE.Aplicacion.Enumerativos;
 
 namespace SGE.Repositorios;
 
-public class RepositorioTramiteTXT : ITramiteRepositorio
+public class RepositorioTramite : ITramiteRepositorio
 {
-  readonly string _nombreArch = "tramites.txt";
 
+  private readonly GestionExpedienteContext _db = new GestionExpedienteContext();
   public List<Tramite> ListarTramites()
   {
-    var listado = new List<Tramite>();
-    using var sr = new StreamReader(_nombreArch);
-    while (!sr.EndOfStream)
-    {
-      var tramite = new Tramite();
-      tramite.Id = int.Parse(sr.ReadLine() ?? "");
-      tramite.ExpedienteId = int.Parse(sr.ReadLine() ?? "");
-      tramite.Etiqueta = (EtiquetaTramite)Enum.Parse(typeof(EtiquetaTramite), (sr.ReadLine() ?? ""));
-      tramite.Contenido = sr.ReadLine() ?? "";
-      tramite.FechaCreacion = DateTime.Parse(sr.ReadLine() ?? "");
-      tramite.UltimaModificacion = DateTime.Parse(sr.ReadLine() ?? "");
-      tramite.IdUsuarioUltimaModificacion = int.Parse(sr.ReadLine() ?? "");
-
-      listado.Add(tramite);
-    }
-    return listado;
+    return _db.Tramites.ToList();
   }
 
-  private int ObtenerUltimoId()
-  {
-    if (!File.Exists(_nombreArch))
-      return 0; // Cuando el archivo no existe
 
-    int ultimoId = 0;
-    using var sr = new StreamReader(_nombreArch);
-    while (!sr.EndOfStream)
-    {
-      ultimoId = int.Parse(sr.ReadLine() ?? "0");
-      sr.ReadLine();
-      sr.ReadLine();
-      sr.ReadLine();
-      sr.ReadLine();
-      sr.ReadLine();
-      sr.ReadLine();
-    }
-    return ultimoId + 1;
-  }
-  private void ReescribirArchivo(List<Tramite> tramites)
+
+  public void TramiteAlta(Tramite tramite)
   {
-    using (StreamWriter sw = new StreamWriter(_nombreArch, false))
-    {
-      foreach (var tramite in tramites)
-      {
-        sw.WriteLine(tramite.Id);
-        sw.WriteLine(tramite.ExpedienteId);
-        sw.WriteLine(tramite.Etiqueta);
-        sw.WriteLine(tramite.Contenido);
-        sw.WriteLine(tramite.FechaCreacion);
-        sw.WriteLine(tramite.UltimaModificacion);
-        sw.WriteLine(tramite.IdUsuarioUltimaModificacion);
-      }
-    }
-  }
-  public void TramiteAlta(Tramite tramite, int idUsuario)
-  {
-    int nuevoId = ObtenerUltimoId();
-    tramite.Id = nuevoId;
+
     tramite.FechaCreacion = DateTime.Now;
     tramite.UltimaModificacion = DateTime.Now;
-    tramite.IdUsuarioUltimaModificacion = idUsuario;
     tramite.Etiqueta = EtiquetaTramite.EscritoPresentado;
+    _db.Add(tramite);
+    _db.SaveChanges();
+    Console.WriteLine("Usuario dado de alta");
 
-    using var sw = new StreamWriter(_nombreArch, true);
-    sw.WriteLine(tramite.Id);
-    sw.WriteLine(tramite.ExpedienteId);
-    sw.WriteLine(tramite.Etiqueta);
-    sw.WriteLine(tramite.Contenido);
-    sw.WriteLine(tramite.FechaCreacion);
-    sw.WriteLine(tramite.UltimaModificacion);
-    sw.WriteLine(tramite.IdUsuarioUltimaModificacion);
   }
   public void TramiteBaja(int id)
   {
 
-    var tramites = ListarTramites();
-    var tramiteAEliminar = tramites.Find(x => x.Id == id);
+    var tramiteAEliminar = TramiteConsultaPorId(id);
     if (tramiteAEliminar != null)
     {
-      tramites.Remove(tramiteAEliminar);
-
-      ReescribirArchivo(tramites);
+      db.Remove(tramiteAEliminar);
+      db.SaveChanges();
     }
   }
-  public void TramiteModificacion(int id, string contenido, EtiquetaTramite etiqueta, int idUsuario)
+  public void TramiteModificacion(int id, Tramite tramite)
   {
-    var tramites = ListarTramites();
-    var index = tramites.FindIndex(t => t.Id == id);
-    if (index >= 0)
+    var tramiteModificar = TramiteConsultaPorId(id);
+    if (tramiteModificar != null)
     {
-      tramites[index].Contenido = contenido;
-      tramites[index].IdUsuarioUltimaModificacion = idUsuario;
-      tramites[index].UltimaModificacion = DateTime.Now;
-      tramites[index].Etiqueta = etiqueta;
-
-      ReescribirArchivo(tramites);
+      tramiteModificar.Contenido = tramite.Contenido;
+      tramiteModificar.UltimaModificacion = DateTime.Now;
+      tramiteModificar.Etiqueta = tramite.Etiqueta;
+      _db.Update(tramiteModificar);
+      _db.SaveChanges();
+      Console.WriteLine("Tramite modificado");
     }
-  }
 
-  public List<Tramite> TramitesPorEtiqueta(EtiquetaTramite etiqueta)
-  {
-    return ListarTramites().Where(t => t.Etiqueta == etiqueta).ToList();
   }
+}
 
-  public List<Tramite> TramiteConsultaTodos()
-  {
-    return ListarTramites();
-  }
+public List<Tramite> TramitesPorEtiqueta(EtiquetaTramite etiqueta)
+{
+  var tramitesEtiqueta = _db.Tramites.Where(t => t.Etiqueta == etiqueta).ToList();
+  return tramitesEtiqueta;
+}
 
-  public Tramite TramiteConsultaPorId(int id)
-  {
-    List<Tramite> tramite = ListarTramites();
-    var index = tramite.FindIndex(t => t.Id == id);
-    if (index < 0)
-      return null;
-    return tramite[index];
-  }
+
+
+public Tramite TramiteConsultaPorId(int id)
+{
+  return _db.Tramites.Where(t => t.Id == id).SingleOrDefault();
 }
