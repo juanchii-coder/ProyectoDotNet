661e9df8153d31b1425a416c34cf5b3c95f80719
juanchii-coder <71896282+juanchii-coder@users.noreply.github.com>
2024-06-19 22:14:52 -0300

    eliminar

--- SGE/SGE.Aplicacion/Interfaces/IValidacionServicio.cs
+++
@@ -1,9 +0,0 @@
-namespace SGE.Aplicacion.Interfaces;
-using SGE.Aplicacion.Entidades;
-public interface IServicioValidacion
-{
-    bool ValidarLogin(string email, string password);
-    bool ValidarUsuario(Usuario usuario);
-    bool ValidarExpediente(int idUsuario, Expediente expediente);
-    bool ValidarTramite(int idusuario, Tramite tramite);
-}
